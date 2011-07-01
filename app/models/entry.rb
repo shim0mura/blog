71a42317f5fdb4b668acb99842607f8c0aa05005
kkosuge <ksgkzy@gmail.com>
2011-07-01 11:22:57 +0900

    kaminari

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,7 @@
-class Content < ActiveRecord::Base
+class Entry < ActiveRecord::Base
+  default_scope :order => 'created_at DESC'
+  paginates_per 5
+  
+  validates_presence_of :title, :path, :text
 end
-
+  
